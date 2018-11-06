739ccf9e8b0ec28fe9791d9d77c10c59a3ea99d9
miz00 <miz.it.related@gmail.com>
2018-11-06 13:42:54 +0900

    create new tweet view

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,34 +1,44 @@
 class TweetsController < ApplicationController
-    before_action :authenticate_user!, only: [:timeline, :new, :create]
+    before_action :authenticate_user!, only: [:new, :create]
 
     #GET /users/1/tweets
-    def index
-
-    end
+    # def index
+    # end
 
     #POST /users/1/tweets
     def create
-
+        @tweet = Tweet.new(tweet_params)
+        respond_to do |format|
+            if @tweet.save
+                redirect_to :back, notice: "tweet was successfully created."
+            else
+                render :new
+            end
+        end
     end
 
     #GET /users/1/tweets/new
     def new
-        
+        @tweet = Tweet.new
     end
 
     #GET /users/1/tweets/:id
-    def show
-
-    end
+    # def show
+    # end
 
     #DELETE /users/1/tweets/:id
     def destroy
 
     end
 
-    #GET /users/1/tweets/timeline
+    #GET /users//tweets/timeline
     def timeline
         
     end
 
+    private
+    def tweet_params
+        params.require(:tweet).permit(:text, :privacy_status).merge(user_id: current_user.id)
+    end
+    
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,6 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :images, dependent: :destroy
   has_many :replies
+
+  validates :text, presence: true
 end
