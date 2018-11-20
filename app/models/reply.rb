a5758ae14430918302c27fe72cbc6b178d29d8e3
miz00 <miz.it.related@gmail.com>
2018-11-20 18:22:22 +0900

    modify views of timeline and user#show

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,3 +1,4 @@
 class Reply < ApplicationRecord
   belongs_to :tweet
+  belongs_to :target_tweet, class_name: 'Tweet', :foreign_key => 'id', :primary_key => 'tweet_id'
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,10 +2,23 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :images, dependent: :destroy
   has_many :replies, dependent: :destroy
+  has_one :my_reply, class_name: 'Reply', :foreign_key => 'tweet_id', :primary_key => 'id'
   has_many :replies, foreign_key: 'target_tweet_id', dependent: :destroy
   has_many :favs, dependent: :destroy
+  #has_one :replied_target_tweet, through: :replies, source: :target_tweet
+  has_one :replied_target_tweet, class_name: 'Reply', :foreign_key => 'target_tweet_id'
 
   validates :text, presence: true
   
   enum privacy_status: { everyone: 1, followers: 2, me: 3 }
+
+  # is_reply?(t.id)
+  def self.is_reply?(tweet_id)
+    Reply.where(tweet_id: tweet_id).present?
+  end
+
+  # has_fav?(t.id,current_user.id)
+  def self.has_fav(tweet_id, user_id)
+    Fav.find_by(tweet_id: tweet_id, user_id: user_id)
+  end
 end
